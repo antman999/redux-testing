3cbfceae9d9464dff60fc13ca87339a966987000
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-14 13:40:40 -0400

    added more tests with descibes

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,35 +1,55 @@
-import React from "react";
-import CommentBox from 'components/CommentBox'
-import {mount} from 'enzyme'
+import React from 'react';
+import CommentBox from 'components/CommentBox';
+import { mount } from 'enzyme';
 
 describe('Test for Component box rendering', () => {
-  //If we use full DOM rendering in enzyme it is using the same JSDOM from react so After use make sure to unmount. Shallow on the other hand doesnt need this function.
+	//If we use full DOM rendering in enzyme it is using the same JSDOM from react so After use make sure to unmount. Shallow on the other hand doesnt need this function.
 
-  let wrapper; 
-  beforeEach(() => {
-     wrapper = mount(<CommentBox />)
-  })
+	let wrapper;
+	beforeEach(() => {
+		wrapper = mount(<CommentBox />);
+	});
 
-  afterEach(() => {
-    wrapper.unmount(); 
-  //This will unmount after each test, cleaning our code 
-  })
+	afterEach(() => {
+		wrapper.unmount();
+		//This will unmount after each test, cleaning our code
+	});
 
-  test('should have a text area and a button', () => {
+	test('should have a text area and a button', () => {
+		expect(wrapper.find('textarea').length).toEqual(1);
+		expect(wrapper.find('button').length).toEqual(1);
+	});
 
-    expect(wrapper.find('textarea').length).toEqual(1)
-    expect(wrapper.find('button').length).toEqual(1);  
-  })
-
-  test('should allow a user to input text into the text area', () => {
-    wrapper.find('textarea').simulate('change', {
-      target: { value: 'new comment' }
-      //^^^ the second value is a mock event,  needs those 2 objects 
+  describe('simulate events', () => {
+    
+    beforeEach(() => {
+      wrapper.find('textarea').simulate('change', {
+				target: { value: 'new comment' },
+				//^^^ the second value is a mock event,  needs those 2 objects
+			});
+			wrapper.update();
     })
-    wrapper.update()
-    //^^We have to force react to update because state is async and will sometimes not update
-    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
-  })
+
+		test('should allow a user to input text into the text area', () => {
+			
+			//^^We have to force react to update because state is async and will sometimes not update
+			expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+		});
+
+    test('should know when the input is submitted, and text area should get emptied', () => {
+      
+			expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+
+			wrapper.find('form').simulate('submit');
+
+			wrapper.update();
+
+			expect(wrapper.find('textarea').prop('value')).toEqual('');
+		});
   
-  
-})
+  });
+
+
+
+
+});
